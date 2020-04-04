94ab7e3c43dc13134173c6284eb4cc4a5c882aec
Matheus Felix <matheusfelixr@hotmail.com>
2020-04-04 00:42:41 -0300

    Projeto conversor de moedas

---
+++ src/components/Converter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import './Converter.css';
+
+export default class Converter extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state={
+            firstCoin_valor:"",
+            secondCoin_valor:0,
+        }
+        this.converter = this.converter.bind(this)
+    }
+
+    converter(){
+        let from_to  = `${this.props.firstCoin}_${this.props.secondCoin}`;
+        let url  = `http://free.currencyconverterapi.com/api/v5/convert?q=${from_to}&compact=y`;
+
+        fetch(url).then(res=>{
+            return res.json()
+        }).then(json=>{
+            let price = json[from_to].val;
+            let secondCoin_valor = (parseFloat(this.state.firstCoin_valor * price).toFixed(2)) 
+            this.setState({secondCoin_valor});
+        })
+    }
+
+    render() {
+        return (
+            <div className="converter">
+                <h2>{this.props.firstCoin} para {this.props.secondCoin}</h2>
+                <input type ="text" onChange={(event)=>{this.setState({firstCoin_valor:event.target.value })}}></input>
+                <input type ="button" value="Converter" onClick={this.converter}></input>
+                <h2>{this.state.secondCoin_valor}</h2>
+            </div>
+        )
+    }
+}
